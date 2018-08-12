cb7de67363334e0a9a91f9319a4aff09d02ff356
AyanC1 <choudhury.ayan13@gmail.com>
2018-08-12 05:14:23 +0000

    volte: fixed volte issues for many carriers

--- device.mk
+++ device.mk
@@ -291,7 +291,17 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, vendor/xiaomi/MiuiCamera/config.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.HAL3.enabled=1
+
+    persist.camera.HAL3.enabled=1 \
+    camera.hal1.packagelist=com.whatsapp \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.radio.rat_on=combine \
+    persist.radio.data_ltd_sys_ind=1 \
+    persist.radio.data_con_rprt=1 \
+    persist.radio.calls.on.ims=1
 # IPv6
 PRODUCT_PACKAGES += \
     ebtables \
@@ -333,3 +343,11 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, device/xiaomi/whyred/treble.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/whyred/whyred-vendor.mk)
+<<<<<<< HEAD
+=======
+
+# Kernel updater app
+#PRODUCT_PACKAGES += \
+ #   Codex
+
+>>>>>>> 14c6457... volte: fixed volte issues for many carriers
